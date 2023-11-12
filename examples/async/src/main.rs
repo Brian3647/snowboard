f55bd01ae95f308ef342e5264fb7fd591aa92234
Brian3647 <victoris3647@gmail.com>
2023-11-12 12:38:42 +0100

    :memo: Fix README

--- examples/async/src/main.rs
+++ examples/async/src/main.rs
@@ -2,8 +2,7 @@ use snowboard::async_std::task;
 use snowboard::{Request, ResponseLike, Result, Server};
 use std::time::Duration;
 
-async fn index(req: Request) -> impl ResponseLike {
-	println!("{:#?}", req);
+async fn index(_: Request) -> impl ResponseLike {
 	task::sleep(Duration::from_secs(1)).await;
 
 	"Async works!"
